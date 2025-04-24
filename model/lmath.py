fixed vanishing gradients in fully hyperbolic block

model/lmath.py
@@ -75,7 +75,7 @@ def inner0(v, *, k, keepdim=False, dim=-1):
 
 @torch.jit.script
 def _inner0(v, k: torch.Tensor, keepdim: bool = False, dim: int = -1):
-    res = -v.narrow(dim, 0, 1) * torch.sqrt(k)
+    res = -v.narrow(dim, 0, 1) * torch.sqrt(torch.exp(k))
     if keepdim is False:
         res = res.squeeze(dim)
     return res
@@ -144,7 +144,7 @@ def dist0(x, *, k, keepdim=False, dim=-1):
 @torch.jit.script
 def _dist0(x, k: torch.Tensor, keepdim: bool = False, dim: int = -1):
     d = -_inner0(x, k=k, dim=dim, keepdim=keepdim)
-    return torch.sqrt(k) * arcosh(d / k)
+    return torch.sqrt(torch.exp(k)) * arcosh(d / torch.exp(k))
 
 
 def project(x, *, k, dim=-1):
@@ -353,8 +353,8 @@ def expmap0(u, *, k, dim=-1):
 @torch.jit.script
 def _expmap0(u, k: torch.Tensor, dim: int = -1):
     nomin = _norm(u, keepdim=True, dim=dim)
-    l_v = torch.cosh(nomin / torch.sqrt(k)) * torch.sqrt(k)
-    r_v = torch.sqrt(k) * torch.sinh(nomin / torch.sqrt(k)) * u / nomin
+    l_v = torch.cosh(nomin / torch.sqrt(torch.exp(k))) * torch.sqrt(torch.exp(k))
+    r_v = torch.sqrt(torch.exp(k)) * torch.sinh(nomin / torch.sqrt(torch.exp(k))) * u / nomin
     dn = r_v.size(dim) - 1
     p = torch.cat((l_v + r_v.narrow(dim, 0, 1), r_v.narrow(dim, 1, dn)), dim)
     return p
@@ -430,7 +430,7 @@ def logmap0(y, *, k, dim=-1):
 @torch.jit.script
 def _logmap0(y, k, dim: int = -1):
     dist_ = _dist0(y, k=k, dim=dim, keepdim=True)
-    nomin_ = 1.0 / k * _inner0(y, k=k, keepdim=True) * torch.sqrt(k)
+    nomin_ = 1.0 / torch.exp(k) * _inner0(y, k=k, keepdim=True) * torch.sqrt(torch.exp(k))
     dn = y.size(dim) - 1
     nomin = torch.cat((nomin_ + y.narrow(dim, 0, 1), y.narrow(dim, 1, dn)), dim)
     denom = _norm(nomin, keepdim=True)

model/model.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from model.lmath import project, distance, norm
+from model.lmath import project, distance, norm, logmap0, expmap0
 from config.model_config import MultiGeomGPTConfig
 
 
@@ -385,10 +385,10 @@ class FullyHyperbolicBlock(nn.Module):
         self.rotary = Rotary(config.head_dim)
 
         self.mlp_expand = nn.Linear(config.n_embd + 1, 4 * config.n_embd + 3)
-        self.act = nn.ReLU()
+        self.act = nn.GELU()
         self.mlp_shrink = nn.Linear(4 * (config.n_embd + 1), config.n_embd)
 
-        # self.normalization = nn.LayerNorm(config.n_embd)
+        self.normalization = nn.LayerNorm(config.n_embd)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -397,8 +397,9 @@ class FullyHyperbolicBlock(nn.Module):
         if x.shape[-1] == self.config.n_embd:
             lx = project(x, k=self.block_curvature) # map euc/sph input to block hyperboloid (B, T, n_embd + 1)
         elif x.shape[-1] == self.config.n_embd + 1:
-            x_norm = norm(x, keepdim=True) # map hyp input x to block hyperboloid (B, T, n_embd + 1)
-            lx = x * torch.sqrt(torch.exp(self.block_curvature)) / x_norm
+            # x_norm = norm(x, keepdim=True) # map hyp input x to block hyperboloid (B, T, n_embd + 1)
+            # lx = x * torch.sqrt(torch.exp(self.block_curvature)) / x_norm
+            lx = x
         else:
             raise ValueError(f"input x has invalid vector dimension: {x.shape[-1]}")
 
@@ -454,9 +455,10 @@ class FullyHyperbolicBlock(nn.Module):
         """
         lx and f_lx are tensors from hyperboloid (B, T, n_embd + 1)
         """
-        skipped = lx + f_lx
-        skipped_norm = norm(skipped, keepdim=True)
-        skipped = skipped * torch.sqrt(torch.exp(self.block_curvature)) / skipped_norm
+        skipped = project(lx.narrow(-1, 1, lx.shape[-1] - 1) + f_lx.narrow(-1, 1, f_lx.shape[-1] - 1), k=self.block_curvature)
+        # skipped = expmap0(logmap0(lx, k=self.block_curvature) + logmap0(f_lx, k=self.block_curvature), k=self.block_curvature)
+        # skipped_norm = norm(skipped, keepdim=True)
+        # skipped = skipped * torch.sqrt(torch.exp(self.block_curvature)) / skipped_norm
 
         return skipped
     
@@ -479,8 +481,8 @@ class FullyHyperbolicBlock(nn.Module):
         lx is a tensor from hyperboloid (B, T, n_embd + 1)
         """
         lx = lx.narrow(-1, 1, lx.shape[-1] - 1)
-        lx = F.rms_norm(lx, (lx.size(-1),))
-        # lx_normed = self.normalization(lx_s)
+        # lx = F.rms_norm(lx, (lx.size(-1),))
+        lx = self.normalization(lx)
         lx = project(lx, k=self.block_curvature)
 
         return lx
@@ -580,7 +582,7 @@ class MultiGeometryGPT(nn.Module):
     def forward(self, idx: torch.Tensor, targets: torch.Tensor = None):
         x = self.transformer.wte(idx)
 
-        for i, block in enumerate(self.transformer.layers):
+        for block in self.transformer.layers:
             x = block(x)
         
         # if the last layer is fully hyperbolic we have to reduce dimension by 1 to get n_embd dimension

train.py
@@ -2,7 +2,7 @@ import sys
 with open(sys.argv[0]) as f:
     code = f.read() # read the code of this file ASAP, for logging
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 import time
 import datetime
@@ -273,7 +273,7 @@ optimizer_matrix = torch.optim.Adam(matrix_params, lr=model_config.matrix_lr, be
 
 opt = Prodigy(lm_head_params + wte_params + matrix_params, lr=1., use_bias_correction=True, weight_decay=0.0)
 optimizers = [optimizer_head, optimizer_matrix, optimizer_wte]
-total_params_list = lm_head_params + wte_params + matrix_params
+total_params_list = lm_head_params + matrix_params
 if len(vector_params) > 0:
     optimizer_vector = torch.optim.Adam(vector_params, lr=model_config.vector_lr, betas=(0.9, 0.999), eps=1e-10, fused=True)
     optimizers.append(optimizer_vector)
@@ -319,7 +319,8 @@ else:
     if master_process:
         print(f"block curvatures and lm_head.k are not learned")
 
-optimizers = [Prodigy(total_params_list, lr=1., use_bias_correction=True, weight_decay=0.0)]
+optimizers = [torch.optim.Adam(wte_params, lr=model_config.wte_lr, betas=(0.9, 0.999), eps=1e-10, fused=True), Prodigy(total_params_list, lr=1., use_bias_correction=True, weight_decay=0.0)]
+# optimizers = [torch.optim.Adam(total_params_list, lr=model_config.matrix_lr, betas=(0.9, 0.999), eps=1e-10, fused=True, weight_decay=0.005)]
 
 # Configure schedulers
 def get_lr(it):
@@ -547,6 +548,13 @@ for step in tqdm(range(train_config.num_iterations + 1)):
         print(f"Step: {step}, grad_norm/attn_k", grad_norm_attn_k)
         if len(vector_params) > 0:
             print(f"Step: {step}, grad_norm/vector", grad_norm_vector)
+        print("=" * 72)
+        grad_norm_wte = compute_grad_norm(raw_model.transformer.wte.parameters())
+        print(f"Grad norm wte: {grad_norm_wte}")
+        for i in range(len(raw_model.transformer.layers)):
+            grad_norm_layer = compute_grad_norm(raw_model.transformer.layers[i].parameters())
+            print(f"Grad norm layer {i}: {grad_norm_layer}")
+        
 
     # step the optimizers and schedulers
     for opt, sched in zip(optimizers, schedulers):